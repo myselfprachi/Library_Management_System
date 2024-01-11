87f5000cbd48cb6a5d7b27b945d55e69523c33bd
Prachi <139076156+myselfprachi@users.noreply.github.com>
2024-01-11 22:03:20 +0530

    student done

--- source.cpp
+++ source.cpp
@@ -7,6 +7,20 @@
 
 using namespace std;
 
+
+class Student{
+private:
+    string name;
+    int id;
+
+
+
+public:
+    void addStudent();
+    void viewDetails();
+};
+
+
 class Login{
 private:
     string username;
@@ -79,19 +93,56 @@ bool Login::registerUser() {
     return true;
 }
 
+void Student::addStudent(){
+    cout<<"Enter the name of the student: ";
+    cin.ignore();
+    getline(cin, name);
+    cout<<"Enter the student ID: ";
+    cin>>id;
+
+    
+    ofstream studentFile("students.txt", ios::app);
+    studentFile<<id<<" "<<name<<endl;
+    studentFile.close();
+
+    cout<<"Student added successfully." <<endl;
+}
+
+void Student::viewDetails(){
+    int studentId;
+    cout<< "Enter the student ID: ";
+    cin>>studentId;
+
+    
+    ifstream studentFile("students.txt");
+    int currentId;
+    string currentName;
+
+    while(studentFile>>currentId>>currentName) {
+        if(currentId==studentId) {
+            cout<<"Student Name: "<<currentName<<endl;
+            cout<< "Student ID: "<<currentId<<endl;
+        
+            break;
+        }
+    }
+
+    studentFile.close();
+}
 
 
-int main() {
+
+int main(){
     Login login;
     bool loggedIn=login.authenticate();
 
-    if(!loggedIn)
-     {
+    if (!loggedIn)
+    {
         char choice;
         cout<<"User not logged in. Do you want to register? (y/n): ";
         cin>>choice;
 
-        if(choice!='y'&&choice!='Y')
+        if(choice!='y'&&choice!='Y') 
         {
             cout<<"Exiting."<<endl;
             return 0;
@@ -99,12 +150,46 @@ int main() {
 
         loggedIn=login.registerUser();
         if(!loggedIn)
-         {
-            cout<<"Registration failed. Exiting." <<endl;
+        {
+            cout<< "Registration failed. Exiting."<<endl;
             return 0;
         }
     }
 
+    Student student;
+
+    int choice;
+    do {
+        cout<<"\n===== Library Management System =====\n";
+        cout<<"------------------------------------------" <<endl;
+        cout<<"------------------------------------------" <<endl;
+        cout<<"1: Add Student"<<endl;
+        cout<<"2: View Student Detail" <<endl;
+        cout<<"Enter your choice (Enter 0 to exit): " <<endl;
+        cout<<"------------------------------------------" <<endl;
+        cout<<"------------------------------------------" <<endl;
+        cin>>choice;
+
+        switch(choice)
+        {
+            case 1:
+                student.addStudent();
+                break;
+
+            case 2:
+                student.viewDetails();
+                break;
+
+            case 0:
+                cout << "Exiting program." << endl;
+                break;
+
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+                break;
+        }
+    } 
+    while(choice != 0);
 
     return 0;
 }
