86d645ef039f4c99a7b6007757167d8330e0b1b0
Prachi <139076156+myselfprachi@users.noreply.github.com>
2024-01-11 22:17:37 +0530

    done

--- source.cpp
+++ source.cpp
@@ -7,6 +7,22 @@
 
 using namespace std;
 
+class Book{
+private:
+    string title;
+    int bookId;
+    bool isIssued;
+
+
+public:
+    void addBook();
+    void displayBooks();
+    void issueBook();
+    void returnBook();
+
+
+
+};
 
 class Student{
 private:
@@ -20,8 +36,8 @@ public:
     void viewDetails();
 };
 
-
-class Login{
+ 
+ class Login{
 private:
     string username;
     string password;
@@ -130,19 +146,133 @@ void Student::viewDetails(){
     studentFile.close();
 }
 
+void Book::addBook() {
+    cout<<"Enter the title of the book: ";
+    cin.ignore();
+    getline(cin, title);
+    cout<<"Enter the book ID: ";
+    cin>>bookId;
+    isIssued=false;
+
+    
+    ofstream bookFile("books.txt", ios::app);
+    if(isIssued){
+
+        bookFile<<bookId<<" "<<title<<"1"<<endl;
+    } else 
+    {
+        bookFile<<bookId<<" "<<title<<" 0"<<endl;
+    }
+    bookFile.close();
+
+    cout<<"Book added successfully." <<endl;
+}
+
+void Book::displayBooks(){
+    ifstream bookFile("books.txt");
+    int currentBookId;
+    string currentTitle;
+    bool currentIsIssued;
+
+    while(bookFile>>currentBookId>>currentTitle>>currentIsIssued){
+        cout<<"Book ID: "<<currentBookId<<"|Title:"<<currentTitle<<" |Status: ";
+        if(currentIsIssued){
+            cout<<"Checked Out" <<endl;
+        } 
+        else 
+        {
+            cout<<"Available"<<endl;
+        }
+
+    }
+
+    bookFile.close();
+}
 
 
-int main(){
+void Book::issueBook(){
+    int bookId;
+    cout<<"Enter the Book ID to issue: ";
+    cin>>bookId;
+
+    ifstream bookFile("books.txt");
+    ofstream tempFile("temp.txt", ios::app);
+
+    int currentBookId;
+    string currentTitle;
+    bool currentIsIssued;
+
+    while(bookFile>>currentBookId>>currentTitle>>currentIsIssued) {
+        if(currentBookId==bookId) {
+            if(currentIsIssued){
+                cout<<"Book is already checked out."<<endl;
+            } 
+            else 
+            {
+                currentIsIssued=true;
+                cout<<"Book issued successfully." <<endl;
+            }
+        }
+        tempFile<<currentBookId<<" "<<currentTitle<<" "<<currentIsIssued<<endl;
+    }
+
+    bookFile.close();
+    tempFile.close();
+
+    
+
+    remove("books.txt");
+    rename("temp.txt", "books.txt");
+}
+
+void Book::returnBook(){
+    int bookId;
+    cout<<"Enter the Book ID to return: ";
+    cin>>bookId;
+
+    ifstream bookFile("books.txt");
+    ofstream tempFile("temp.txt", ios::app);
+
+    int currentBookId;
+    string currentTitle;
+    bool currentIsIssued;
+
+    while(bookFile>>currentBookId>>currentTitle>>currentIsIssued) 
+    {
+        if(currentBookId==bookId) 
+        {
+            if(!currentIsIssued)
+             {
+                cout<<"Book is not checked out." <<endl;
+            } 
+            else
+            {
+                currentIsIssued=false;
+                cout<<"Book returned successfully." <<endl;
+            }
+        }
+        tempFile<<currentBookId<<" "<<currentTitle<<" "<<currentIsIssued <<endl;
+    }
+
+    bookFile.close();
+    tempFile.close();
+
+    remove("books.txt");
+    rename("temp.txt", "books.txt");
+}
+
+
+int main() {
     Login login;
     bool loggedIn=login.authenticate();
 
-    if (!loggedIn)
-    {
+    if(!loggedIn)
+     {
         char choice;
         cout<<"User not logged in. Do you want to register? (y/n): ";
         cin>>choice;
 
-        if(choice!='y'&&choice!='Y') 
+        if(choice!='y'&&choice!='Y')
         {
             cout<<"Exiting."<<endl;
             return 0;
@@ -150,24 +280,30 @@ int main(){
 
         loggedIn=login.registerUser();
         if(!loggedIn)
-        {
-            cout<< "Registration failed. Exiting."<<endl;
+         {
+            cout<<"Registration failed. Exiting." <<endl;
             return 0;
         }
     }
 
     Student student;
+    Book book;
 
     int choice;
-    do {
+    do{
+        
         cout<<"\n===== Library Management System =====\n";
-        cout<<"------------------------------------------" <<endl;
-        cout<<"------------------------------------------" <<endl;
+        cout<<"------------------------------------------"<<endl;
+        cout<<"------------------------------------------"<<endl;
         cout<<"1: Add Student"<<endl;
-        cout<<"2: View Student Detail" <<endl;
-        cout<<"Enter your choice (Enter 0 to exit): " <<endl;
-        cout<<"------------------------------------------" <<endl;
-        cout<<"------------------------------------------" <<endl;
+        cout<<"2: View Student Detail"<<endl;
+        cout<<"3: Add Book"<<endl;
+        cout<<"4: Display Books"<<endl;
+        cout<<"5: Issue Book "<<endl;
+        cout<<"6: Return Book"<<endl;
+        cout<<"Enter your choice (Enter 0 to exit): "<<endl;
+        cout<<"------------------------------------------"<<endl;
+        cout<<"------------------------------------------"<<endl;
         cin>>choice;
 
         switch(choice)
@@ -175,21 +311,36 @@ int main(){
             case 1:
                 student.addStudent();
                 break;
-
+            
             case 2:
                 student.viewDetails();
                 break;
-
-            case 0:
-                cout << "Exiting program." << endl;
+            
+            case 3:
+                book.addBook();
                 break;
-
+            
+            case 4:
+                book.displayBooks();
+                break;
+            
+            
+            case 5:
+                book.issueBook();  
+                break;
+            
+            case 6:
+                book.returnBook();  
+                break;
+            case 0:
+                cout<<"Exiting program." <<endl;
+                break;
             default:
-                cout << "Invalid choice. Please try again." << endl;
+                cout<<"Invalid choice. Please try again."<<endl;
                 break;
         }
     } 
-    while(choice != 0);
+    while(choice!=0);
 
     return 0;
 }
